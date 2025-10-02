cc2c8317181b65037b53fc0ce21642363b0c1d69
connorbroad <connor.broad@gmail.com>
2025-10-02 13:46:46 +1300

    Update DiceRoller.svelte

--- src/components/games/solo-rpg/DiceRoller.svelte
+++ src/components/games/solo-rpg/DiceRoller.svelte
@@ -377,6 +377,7 @@
         border-radius: 8px;
         box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
         min-width: 300px;
+        max-width: 350px;
         text-align: center;
         position: relative;
     }
