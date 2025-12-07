812f16cad5285a52f6333446f524d9dcfe6cf393
Connor Broad <connor.broad@gmail.com>
2025-12-07 15:59:59 +1300

    New paint icon

--- src/components/games/solo-rpg/map/FloatingBrushModeToggle.svelte
+++ src/components/games/solo-rpg/map/FloatingBrushModeToggle.svelte
@@ -56,9 +56,10 @@
                 stroke="currentColor"
                 stroke-width="2">
                 <!-- Paint brush icon -->
-                <path d="M19 11H5a4 4 0 0 0-4 4v0a2 2 0 0 0 2 2h2.5a1.5 1.5 0 0 1 0 3H4" />
-                <path d="M19 11V6a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v5" />
-                <path d="M19 11a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2" />
+                <path
+                    d="M18.37 2.63 14 7l-1.59-1.59a2 2 0 0 0-2.82 0L8 7l9 9 1.59-1.59a2 2 0 0 0 0-2.82L17 10l4.37-4.37a2.12 2.12 0 1 0-3-3Z" />
+                <path d="M9 8c-2 3-4 3.5-7 4l8 10c2-1 6-5 6-7" />
+                <path d="M14.5 17.5 4.5 15" />
             </svg>
             <span class="toggle-label">Paint</span>
         </button>
